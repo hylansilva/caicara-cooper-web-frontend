efaa66c78b2f2a05ed58f31313f8806628130919
Hylan Silva <hylan.00000847839@unicap.br>
2023-12-10 23:29:25 -0300

    Feat: pagina principal e refactor no projeto

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Login } from './pages/login/index.tsx'
+import { Home } from './pages/home/index.tsx'
+import { Dashboard } from './pages/dashboard/index.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: (
+      <h1>Not Found</h1>
+    )
+  }, {
+    path: '/login',
+    element: <Login />,
+    errorElement: (
+      <h1>Not Found</h1>
+    )
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+    errorElement: (
+      <h1>Not Found</h1>
+    )
+  }
+])
+
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
