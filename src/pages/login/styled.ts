7ace41fdb592266e2d5c375396994997ef4df359
Hylan Silva <hylan.00000847839@unicap.br>
2023-12-10 23:30:34 -0300

    Feat: criando a rota e tela de login

---
+++ src/pages/login/styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Image = styled.div`
+    background-image: url('public/images/login_backgroundImage.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100dvh;
+    height: 100vh;
+    `;
